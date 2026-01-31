d7b8176e798df3213cddc658898c0b01c8b75ce0
limits1214 <syl@SYL35EC>
2026-01-31 16:55:01 +0900

    [chore] Copy배치시에 폴더가  없으면 만들어주는 I옵션 추가, 복사되는곳이 파일이 아닌 폴더라고 명시

--- Copy.bat
+++ Copy.bat
@@ -1,5 +1,5 @@
 // 명령어				옵션			원본파일이 있는 위치			사본파일을 저장할 위치
 
-xcopy				/y			.\Engine\Header\*.h			.\Reference\Header
-xcopy				/y			.\Engine\Bin\Engine.dll		.\Client\Bin
-xcopy				/y			.\Engine\Bin\Engine.lib		.\Reference\Library
+xcopy				/y/I			.\Engine\Header\*.h			.\Reference\Header\
+xcopy				/y/I			.\Engine\Bin\Engine.dll		.\Client\Bin\
+xcopy				/y/I			.\Engine\Bin\Engine.lib		.\Reference\Library\
