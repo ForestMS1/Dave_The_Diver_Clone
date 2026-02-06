2aedf4b2b1696467521fb5960a55b88abc7fc188
sungmin <tjdalsjang@gmail.com>
2026-02-06 22:48:46 +0900

    [chore] 파티클 디버깅용 코드 제거 #43

--- Client/Code/CDynamicCamera.cpp
+++ Client/Code/CDynamicCamera.cpp
@@ -110,9 +110,7 @@ void CDynamicCamera::Key_Input(const _float& fTimeDelta)
         m_vEye -= vLength;
         m_vAt -= vLength;
     }
-    if (CDInputMgr::GetInstance()->Mouse_Down(DIM_LB)) {
-        CParticleMgr::GetInstance()->spwan_Particle(BULLET, { 0,0,0 }, 1);
-    }
+   
     if (CDInputMgr::GetInstance()->Get_DIKeyState(DIK_TAB) & 0x80)
     {
         if (m_bCheck)
