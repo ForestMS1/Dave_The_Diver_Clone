eb634369aefd63996e1ee02c8c0086c9b2c3c0f9
daesung <dasung56@gmail.com>
2026-03-04 22:05:53 +0900

    [FEAT] 멈춤 저항일때도 수심 증감

--- SRClient/Code/CDiveDaveIdle.cpp
+++ SRClient/Code/CDiveDaveIdle.cpp
@@ -105,6 +105,30 @@ _int CDiveDaveIdle::Update_State(const _float& fTimeDelta)
 		_vec3 vLastDir = m_pOwner->Get_LastMoveDir();
 		m_pOwner->Move(&vLastDir, fTimeDelta, m_fIdleMoveLastSpeed);
 		m_fIdleMoveLastSpeed -= fTimeDelta * 5.f;
+
+		//수심 증감
+		if (vLastDir.y > 0)
+		{
+			if (abs(vLastDir.x) > 0)
+			{
+				m_pOwner->Change_Depth(-0.01f * 0.8f * m_fIdleMoveLastSpeed);
+			}
+			else
+			{
+				m_pOwner->Change_Depth(-0.01f * m_fIdleMoveLastSpeed);
+			}
+		}
+		else if (vLastDir.y < 0)
+		{
+			if (abs(vLastDir.x) > 0)
+			{
+				m_pOwner->Change_Depth(0.01f * 0.8f * m_fIdleMoveLastSpeed);
+			}
+			else
+			{
+				m_pOwner->Change_Depth(0.01f * m_fIdleMoveLastSpeed);
+			}
+		}
 	}
 	else
 		m_fIdleMoveLastSpeed = 0.f;
