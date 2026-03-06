531d627d24b1e66c13764812a16a5f23ca9655d1
sungmin <tjdalsjang@gmail.com>
2026-03-06 16:40:33 +0900

    [chore] conflict resolve #174

--- SRClient/Code/CShip.cpp
+++ SRClient/Code/CShip.cpp
@@ -642,28 +642,8 @@ void CShip::Render_Scene()
 	}
 
 
-	if (CGameMemMgr::GetInstance()->Get_DiveInfos().size() == 0 && !m_bTalking) {
-		m_fConvAppearTimer += fTimeDelta;
-		if (m_fConvAppearTimer > 1.f) {
-			if (auto pLayer = CManagement::GetInstance()->Get_Scene()->Get_Layer(L"0_GameLogic_Layer"))
-			{
-				if (auto pObj = pLayer->Get_GameObjectFirst(L"DaveConversation"))
-				{
-					pObj->Set_DeadCascade();
-				}
-				else
-				{
-					CDaveConversation* pDaveConversation = CDaveConversation::Create(0.f, -2.f);
-					pDaveConversation->SetCurrentConversation(CDaveConversation::CONVERSATION::CONV_1);
-					pLayer->Add_GameObject(L"DaveConversation", pDaveConversation);
-					m_fConvAppearTimer = 0;
-					m_bTalking = true;
-				}
-			}
-		}
-	}
-	CParticleMgr::GetInstance()->Update_Particle(fTimeDelta);
-	return iExit;
+
+#endif
 }
 
 void CShip::LateUpdate_Scene(const _float& fTimeDelta)
