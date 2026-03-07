947cb45cbe32340bb932380b778a69da95a290bc
limits1214 <syl@SYL35EC>
2026-03-08 05:48:53 +0900

    [feat] fade는 오소그래픽적용

--- SRClient/Code/CTransitionFade.cpp
+++ SRClient/Code/CTransitionFade.cpp
@@ -69,58 +69,65 @@ void		CTransitionFade::LateUpdate_GameObject(const _float& fTimeDelta)
     }
 }
 
-void		CTransitionFade::Render_GameObject()
+void CTransitionFade::Render_GameObject()
 {
     LPDIRECT3DDEVICE9 pGraphicDev = CGraphicDev::GetInstance()->Get_GraphicDev();
+
+    // 1. 기존 행렬(View, Proj) 백업
+    D3DXMATRIX matOldView, matOldProj;
+    pGraphicDev->GetTransform(D3DTS_VIEW, &matOldView);
+    pGraphicDev->GetTransform(D3DTS_PROJECTION, &matOldProj);
+
+    // 2. 오소그래픽 및 뷰 행렬 설정 (UI 모드)
+    D3DXMATRIX matView, matProj;
+    D3DXMatrixIdentity(&matView); // 카메라는 원점에 고정
+    D3DXMatrixOrthoLH(&matProj, (float)WINCX, (float)WINCY, 0.f, 1.f);
+
+    pGraphicDev->SetTransform(D3DTS_VIEW, &matView);
+    pGraphicDev->SetTransform(D3DTS_PROJECTION, &matProj);
+
+    
     float alphaRatio = m_fAlpha;
-    DWORD tfactor = D3DCOLOR_ARGB(
-        (BYTE)(alphaRatio * 255.f),
-        255, 255, 255
-    );
-    //pGraphicDev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
-    //pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+    DWORD tfactor = D3DCOLOR_ARGB((BYTE)(alphaRatio * 255.f), 255, 255, 255);
+
     pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
     pGraphicDev->SetRenderState(D3DRS_TEXTUREFACTOR, tfactor);
 
-        pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
+   
+    pGraphicDev->SetTransform(D3DTS_WORLD, m_pTransformCom->Get_World());
 
-        if (auto vecAsset = CAssetMgr::GetInstance()->Get_Asset(L"Tex_Transition_Fade_Black"))
-        {
-            if (auto pTexture = dynamic_cast<CAssetTexture*>(vecAsset->at(0)))
-            {
-                pGraphicDev->SetTexture(0, pTexture->Get_Texture());
-            }
-        }
+    if (auto vecAsset = CAssetMgr::GetInstance()->Get_Asset(L"Tex_Transition_Fade_Black"))
+    {
+        if (auto pTexture = dynamic_cast<CAssetTexture*>(vecAsset->at(0)))
+            pGraphicDev->SetTexture(0, pTexture->Get_Texture());
+    }
 
-        // COLOR = Texture * TFACTOR
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_TFACTOR);
+    // Texture Stage States (TFACTOR 사용)
+    pGraphicDev->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+    pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+    pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_TFACTOR);
 
-        // ALPHA = TextureAlpha * TFACTORAlpha
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_TFACTOR);
-        m_pBufferCom->Render_Buffer();
+    pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+    pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+    pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_TFACTOR);
 
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE); // 보통 기본값도 MODULATE인 경우가 많지만 확인 필요
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE); // TFACTOR를 DIFFUSE로 원복
+    m_pBufferCom->Render_Buffer();
 
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
-        pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE); // TFACTOR를 DIFFUSE로 원복
+    // 스테이트 원복
+    pGraphicDev->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
+    pGraphicDev->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
 
+    D3DXMATRIX matIdentity;
+    D3DXMatrixIdentity(&matIdentity);
+    pGraphicDev->SetTransform(D3DTS_WORLD, &matIdentity);
 
-
-        D3DXMATRIX matTmp;
-        D3DXMatrixIdentity(&matTmp);
-        pGraphicDev->SetTransform(D3DTS_WORLD, &matTmp);
-
-    //pGraphicDev->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
     pGraphicDev->SetRenderState(D3DRS_TEXTUREFACTOR, 0xFFFFFFFF);
     pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
-    //pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);
+    // --- 기존 렌더링 로직 끝 ---
+
+    // 3. 기존 행렬 복구 (다시 3D 세상을 그릴 수 있도록)
+    pGraphicDev->SetTransform(D3DTS_VIEW, &matOldView);
+    pGraphicDev->SetTransform(D3DTS_PROJECTION, &matOldProj);
 }
 
 HRESULT			CTransitionFade::Ready_Component()
