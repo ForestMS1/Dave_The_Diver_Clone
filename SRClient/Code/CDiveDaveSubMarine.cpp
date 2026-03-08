e5e26c87c9b2b0fa9ef0325d43ac282aec3e44c9
daesung <dasung56@gmail.com>
2026-03-08 11:12:24 +0900

    [FEAT] 잠수함-물고기 충돌 사운드 추가

--- SRClient/Code/CDiveDaveSubMarine.cpp
+++ SRClient/Code/CDiveDaveSubMarine.cpp
@@ -7,6 +7,7 @@
 #include "CAssetTexture.h"
 #include "CColliderMgr.h"
 #include "CFishGameObject.h"
+#include "CSoundMgr.h"
 CDiveDaveSubMarine::CDiveDaveSubMarine(CDiveDave* pOwner)
 	:CBaseState<CDiveDave>(pOwner)
 {
@@ -123,7 +124,17 @@ void CDiveDaveSubMarine::LateUpdate_State(const _float& fTimeDelta)
 			{
 				if (pCollider->Get_Tag() == L"AABB_FishHitbox")
 				{
-					reinterpret_cast<CFishGameObject*>(pCollider->Get_VoidPtr())->Damaged(100000);
+					CFishGameObject* pFish = reinterpret_cast<CFishGameObject*>(pCollider->Get_VoidPtr());
+					if (pFish->Get_FishState() != Fish::FS_DIE)
+					{
+						if (!CSoundMgr::GetInstance()->IsChannelPlaying(CSoundMgr::SFX_DIVE_SUBMARINE_COLL1))
+							CSoundMgr::GetInstance()->PlaySoundOne(L"Sound_SubMarine_Coll", CSoundMgr::SFX_DIVE_SUBMARINE_COLL1, 1.f);
+						//else if (!CSoundMgr::GetInstance()->IsChannelPlaying(CSoundMgr::SFX_DIVE_SUBMARINE_COLL2))
+						//	CSoundMgr::GetInstance()->PlaySoundOne(L"Sound_SubMarine_Coll", CSoundMgr::SFX_DIVE_SUBMARINE_COLL2, 1.f);
+						//else if (!CSoundMgr::GetInstance()->IsChannelPlaying(CSoundMgr::SFX_DIVE_SUBMARINE_COLL3))
+						//	CSoundMgr::GetInstance()->PlaySoundOne(L"Sound_SubMarine_Coll", CSoundMgr::SFX_DIVE_SUBMARINE_COLL3, 1.f);
+					}
+					pFish->Damaged(100000);
 				}
 			}
 		}
